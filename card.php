8623d1bc87842a2878e1e4d8333daa79ce131a88
xmlscript <xmlscript@gmail.com>
2018-05-08 19:11:50 +0800

    dev

--- card.php
+++ card.php
@@ -2,6 +2,8 @@
 
 class card{
 
+  public $timestamp, $nonceStr, $signType, $cardSign;
+
   /**
    * jssdk的wx.chooseCard()需要用到的部分参数
    * 另外由于历史原因，卡券的JS接口先于JSSDK出现，当时的JSAPI并没有鉴权体系，所以在卡券的签名里也加上了appsecret/api_ticket这些身份信息，希望开发者理解。
@@ -9,24 +11,20 @@ class card{
    * @see https://mp.weixin.qq.com/debug/cgi-bin/sandbox?t=cardsign
    * @see https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421141115
    */
-  function chooseCard(string $id=null, string $code=null, string $openid=null, string $balance=null):array{
-    return [
-      //'shopId' => '', #用于筛选出拉起带有指定location_list(shopID)的卡券列表
-      //'cardType' => '', #用于拉起指定卡券类型的卡券列表。为空时，默认拉起所有卡券的列表
-      'cardId' => $id, #用于拉起指定cardId的卡券列表，为空时，默认拉起所有卡券的列表
-      'timestamp' => $timestamp=time(),
-      'nonceStr' => $nonceStr=md5($time+$_SERVER['REQUEST_TIME_FLOAT']),
-      'signType' => 'SHA1',
-      'cardSign' => $this->signature(
-        new ticket($this->token,'wx_card'),
-        $timestamp,
-        $nonceStr,
-        $id,
-        $code,
-        $openid,
-        $balance
-      )
-    ];
+  function __construct(token $token, string $id=null, string $code=null, string $openid=null, string $balance=null){
+    $this->timestamp = time();
+    $this->nonceStr = md5($this->timestamp+$_SERVER['REQUEST_TIME_FLOAT']);
+    $this->signType = 'SHA1';
+    $this->cardSign = $this->signature(
+      new ticket($token,'wx_card'),
+      $this->timestamp,
+      $this->nonceStr,
+      $id,
+      $code,
+      $openid,
+      $balance
+    );
+
   }
 
   private function signature(string ...$arr):string{
