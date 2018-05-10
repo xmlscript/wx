7a7e9a3d1592413e785861b6d2ffc03b318af949
xmlscript <xmlscript@gmail.com>
2018-05-10 19:45:06 +0800

    dev

--- config.php
+++ config.php
@@ -21,20 +21,4 @@ class config{
     $this->signature = sha1(urldecode(http_build_query($arr)));//不能转义，所以urldecode中和一下
   }
 
-  static function construct(string $url){
-    $arr = [
-      'jsapi_ticket' => 't',
-      'noncestr' => md5($_SERVER['REQUEST_TIME_FLOAT']),
-      'timestamp' => $_SERVER['REQUEST_TIME'],
-      'url' => $url,
-    ];
-    ksort($arr);//手动按顺序整理好了，这一步多余
-    return [
-    'appid' => $token->appid,
-    'timestamp' => $arr['timestamp'],
-    'nonceStr' => $arr['noncestr'],
-    'signature' => sha1(urldecode(http_build_query($arr)))
-    ];
-  }
-
 }
