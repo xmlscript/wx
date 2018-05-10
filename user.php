a63d6741444d36dca4e5cefcb1376516699dab11
xmlscript <xmlscript@gmail.com>
2018-05-10 12:54:51 +0800

    dev

--- user.php
+++ user.php
@@ -7,9 +7,9 @@ class user{
   public $openid, $nickname, $sex, $province, $city, $country, $headimgurl, $privilege, $unionid;
 
   /**
-   * @param string $lang zh_CN | zh_TW | en
+   * @param string $lang='en' zh_CN | zh_TW 胡乱填写还是默认en，中国显示China
    */
-  function __construct(token $token, string $lang='zh_CN'){
+  function __construct(token $token, string $lang='en'){
     foreach($this->check(request::url($token::HOST.'/sns/userinfo')
       ->fetch(['access_token'=>$token->access_token,'openid'=>$token->openid,'lang'=>$lang])
       ->json()) as $k=>$v)
