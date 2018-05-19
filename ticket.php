256c2040fcce6de025113978820d9963bc2f3297
xmlscript <xmlscript@gmail.com>
2018-05-19 13:53:01 +0800

    dev

--- ticket.php
+++ ticket.php
@@ -10,6 +10,8 @@ class ticket{
 
   public $ticket;
   
+  //FIXME 没有约束type可能导致被cache吞噬的异常
+  //FIXME 别处刷ticket导致提前过期，尚未测试错误码，可能是9001003，则应该手动清除缓存
   final function __construct(\mp\token $token, string $type){
     $this->ticket = new cache($token->appid.$type, $token->appid, 7200, function() use ($token,$type){
       return request::url(token::HOST.'/cgi-bin/ticket/getticket')
